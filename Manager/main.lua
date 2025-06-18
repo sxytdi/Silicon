4e59cc80b196a74acba2dc2ddd4a5cfc2a3e738a
Jake <129622095+LuaFunction@users.noreply.github.com>
2025-06-18 18:40:50 +0100

    Update main.lua

--- Manager/main.lua
+++ Manager/main.lua
@@ -1,28 +1,28 @@
 if game.PlaceId == (18722757961) then
 wait(0.1)
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/Ragblood/main.lua"),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/Ragblood/main.lua"),true))()
 
 elseif game.PlaceId == (891852901) then
 wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/Greenville/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/Greenville/main.lua", true))()
 
 elseif game.PlaceId == (8416011646) then
 wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/MapleCounty/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/MapleCounty/main.lua", true))()
 
 elseif game.PlaceId == (2534724415) then
 wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/ERLC/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/ERLC/main.lua", true))()
 
 elseif game.PlaceId == (4637668954) then
 wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/RensselaerCounty/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/RensselaerCounty/main.lua", true))()
 
 elseif game.PlaceId == (87720117989607) then
 wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/Ridgewood/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/Ridgewood/main.lua", true))()
 
 elseif game.PlaceId == (5104202731) then
 wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Quantum/refs/heads/main/SouthwestFlorida/main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaFunction/Silicon/refs/heads/main/SouthwestFlorida/main.lua", true))()
 end
