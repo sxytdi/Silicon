4bbfc3811550c452f4dfd499ec7808039b584ebb
Jake <129622095+sxyf82@users.noreply.github.com>
2025-07-13 12:01:28 +0100

    Update freeapi.lua

--- Manager/freeapi.lua
+++ Manager/freeapi.lua
@@ -1,1 +1,12 @@
-fapi
+if game.PlaceId == (5104202731) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Ragblood/main.lua'),true))()
+  
+elseif game.PlaceId == (891852901) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/main.lua'),true))()
+  
+elseif game.PlaceId == (13415640045) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/main.lua'),true))()
+
+elseif game.PlaceId == (4637668954) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/main.lua'),true))()
+end
