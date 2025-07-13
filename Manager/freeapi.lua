e2fedbb445ec4bc2e56fda044d197e40f4d82ed0
Jake <129622095+sxyf82@users.noreply.github.com>
2025-07-13 12:22:10 +0100

    Update freeapi.lua

--- Manager/freeapi.lua
+++ Manager/freeapi.lua
@@ -2,7 +2,7 @@ if game.PlaceId == (18722757961) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Ragblood/main.lua'),true))()
   
 elseif game.PlaceId == (891852901) then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/main.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/Free/main.lua'),true))()
   
 elseif game.PlaceId == (126884695634066) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Grow%20A%20Garden/main.lua'),true))()
