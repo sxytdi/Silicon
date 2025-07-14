a1ca115adc9535cf34e2bc5e17680d30d06b69bf
Jake <129622095+sxyf82@users.noreply.github.com>
2025-07-14 22:06:34 +0100

    Update freeapi.lua

--- Manager/freeapi.lua
+++ Manager/freeapi.lua
@@ -3,10 +3,4 @@ loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/
   
 elseif game.PlaceId == (891852901) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/Free/main.lua'),true))()
-  
-elseif game.PlaceId == (126884695634066) then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Grow%20A%20Garden/main.lua'),true))()
-
-elseif game.PlaceId == (4637668954) then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Rensselaer%20County/main.lua'),true))()
 end
