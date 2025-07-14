447a5fae8ed864b3ed8f99e71f74c9c5c0d6a680
Jake <129622095+sxyf82@users.noreply.github.com>
2025-07-14 22:06:58 +0100

    Update api.lua

--- Manager/api.lua
+++ Manager/api.lua
@@ -1,3 +1,9 @@
 if game.PlaceId == (891852901) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/main.lua'),true))()
+
+elseif game.PlaceId == (126884695634066) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Grow%20A%20Garden/main.lua'),true))()
+
+elseif game.PlaceId == (4637668954) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Rensselaer%20County/main.lua'),true))()
 end
