b258a8bd9b7f6d0a5f53aece388feff58019a5d3
Jake <129622095+sxyf82@users.noreply.github.com>
2025-07-26 13:57:54 +0100

    Delete Manager/freeapi.lua

--- Manager/freeapi.lua
+++
@@ -1,6 +0,0 @@
-if game.PlaceId == (18722757961) then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Ragblood/main.lua'),true))()
-  
-elseif game.PlaceId == (891852901) then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/sxyf82/Silicon/refs/heads/main/Greenville/Free/main.lua'),true))()
-end
