885676bd32d42f608f59a51ab36d82fbca6837d1
5xy <129622095+sxytdi@users.noreply.github.com>
2025-11-01 19:55:40 +0000

    Implement game-specific loading for scripts
    Add game-specific scripts for various PlaceIds

---
+++ Manager/free.lua
@@ -0,0 +1,15 @@
+if game.PlaceId == (654732683) then -- Car Crushers 2
+loadstring(game:HttpGet("https://bit.ly/4nxrESE"))()
+  
+elseif game.PlaceId == (72712036210947) then -- Fix It Up
+loadstring(game:HttpGet("https://bit.ly/4hXnUch"))()
+  
+elseif game.PlaceId == (891852901) then -- Greenville
+loadstring(game:HttpGet("https://bit.ly/4ok7xJ9"))()
+
+elseif game.PlaceId == (18722757961) then -- Ragblood
+loadstring(game:HttpGet("https://bit.ly/3JxcNKb"))()
+end
+
+-- What the 5xy
+-- What you doin here bud.
