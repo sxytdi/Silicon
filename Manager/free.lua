d273e4039c67cf108daa3d8b055c0b322a70a066
5xy <129622095+sxytdi@users.noreply.github.com>
2025-11-02 00:36:08 +0000

    Update free.lua

--- Manager/free.lua
+++ Manager/free.lua
@@ -1,17 +1,17 @@
-if game.PlaceId == (654732683) then -- Car Crushers 2
-loadstring(game:HttpGet("https://bit.ly/4nxrESE"))()
-  
-elseif game.PlaceId == (72712036210947) then -- Fix It Up
-loadstring(game:HttpGet("https://bit.ly/4hXnUch"))()
-  
-elseif game.PlaceId == (891852901) then -- Greenville
-loadstring(game:HttpGet("https://bit.ly/4ok7xJ9"))()
+if game.PlaceId == 654732683 then -- Car Crushers 2
+    loadstring(game:HttpGet("https://bit.ly/4nxrESE"))()
 
-elseif game.PlaceId == (18722757961) then -- Ragblood
-loadstring(game:HttpGet("https://bit.ly/3JxcNKb"))()
+elseif game.PlaceId == 72712036210947 then -- Fix It Up
+    loadstring(game:HttpGet("https://bit.ly/4hXnUch"))()
 
-elseif
-  print("Wrong Game Mush. -5xy")
+elseif game.PlaceId == 891852901 then -- Greenville
+    loadstring(game:HttpGet("https://bit.ly/4ok7xJ9"))()
+
+elseif game.PlaceId == 18722757961 then -- Ragblood
+    loadstring(game:HttpGet("https://bit.ly/3JxcNKb"))()
+
+else
+    print("wrong game mush") -- This prints if none of the above match
 end
 
 -- What the 5xy
