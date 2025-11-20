9bc3c3452ec0e33c1d3891dca5b05c536a209df0
5xy <129622095+sxytdi@users.noreply.github.com>
2025-11-20 20:50:42 +0000

    Update source.lua

--- UI/source.lua
+++ UI/source.lua
@@ -1629,7 +1629,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 	LoadingFrame.Subtitle.Text = Settings.LoadingSubtitle or "Interface Suite"
 
 	if Settings.LoadingTitle ~= "Rayfield Interface Suite" then
-		LoadingFrame.Version.Text = "Rayfield UI"
+		LoadingFrame.Version.Text = "Modded Rayfield by Silicon"
 	end
 
 	if Settings.Icon and Settings.Icon ~= 0 and Topbar:FindFirstChild('Icon') then
