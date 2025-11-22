0a45ad559b74b94aba8d13533bb906596375d430
5xy <129622095+sxytdi@users.noreply.github.com>
2025-11-22 00:18:57 +0000

    Add support for additional game PlaceIds

--- Manager/paid.lua
+++ Manager/paid.lua
@@ -1,9 +1,13 @@
-    if game.PlaceId == 891852901 then
+    if game.PlaceId == 891852901 then -- gv
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6c1a31ca488543d8efd71f78177a7566.lua"))()
-    elseif game.PlaceId == 4637668954 then
+    elseif game.PlaceId == 4637668954 then -- rc
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3cc82a592e165b81981ca0da7906b0d4.lua"))()
-    elseif game.PlaceId == 135155039067698 then
+    elseif game.PlaceId == 135155039067698 then -- mx world
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4e46381b1266a81f6b2f44973e084714.lua"))()
-    elseif game.PlaceId == 5104202731 then
+    elseif game.PlaceId == 5104202731 then -- swfl
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f2f649c1e99cf64b62bac3d9bc54da13.lua"))()
+    elseif game.PlaceId == 136801880565837 then -- flick
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b3547311a217f31a70bd9276c76ff85a.lua"))()
+    elseif game.PlaceId == 286090429 then -- arsenal
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/40f9d8677bb2889680de95005200c13e.lua"))()
     end
