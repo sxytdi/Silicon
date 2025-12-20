ceb93b2b0ec701eebb4722693b60ab0091d9acf8
5xy <129622095+sxytdi@users.noreply.github.com>
2025-12-20 09:30:22 +0000

    Update nexusfiddler.lua

--- nexusfiddler.lua
+++ nexusfiddler.lua
@@ -1,10 +1,2 @@
-local Remote = Instance.new("RemoteEvent")
-Remote.Name = "BanMe"
-Remote.Parent = game:GetService("ReplicatedStorage")
-
-Remote.OnServerEvent:Connect(function(player)
-    warn("[ANTI-EXPLOIT] "..player.Name.." ("..player.UserId..") tried to fire BanMe")
-    player:Kick("\n\nBYPASSING DETECTED\n\nYou have been permanently banned for attempting to exploit.\nReason: Fired protected remote 'BanMe'\n\nAppeal at: nowhere lmao")
-end)
-
-print("BanMe trap loaded - exploiters = rekt")
+game.Players.LocalPlayer:Kick("5xy's Secret Message\nNexus tried to exploit us but failed,\nbetter luck next time. LOL\n\ndiscord.gg/siliconxploit")
+game.ReplicatedStorage.Remote.BanMe:FireServer()
