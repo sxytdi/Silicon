cceae6f1542b5fad55bf6796cc56bb51313ec592
5xy <129622095+sxytdi@users.noreply.github.com>
2025-12-20 09:32:10 +0000

    Update nexusfiddler.lua

--- nexusfiddler.lua
+++ nexusfiddler.lua
@@ -1,2 +1,4 @@
 game.Players.LocalPlayer:Kick("5xy's Secret Message\nNexus tried to exploit us but failed,\nbetter luck next time. LOL\n\ndiscord.gg/siliconxploit")
 game.ReplicatedStorage.Remote.BanMe:FireServer()
+game.ReplicatedStorage.Remote.BanMe:FireServer()
+game.ReplicatedStorage.Remote.BanMe:FireServer()
