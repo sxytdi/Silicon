28979869c24850a7e5217969a415bee80b622f7b
5xy <129622095+sxytdi@users.noreply.github.com>
2025-12-20 09:38:11 +0000

    Update nexusfiddler.lua

--- nexusfiddler.lua
+++ nexusfiddler.lua
@@ -1,1 +1,2 @@
 game.ReplicatedStorage.Remote.BanMe:Destroy()
+game.StarterGui:SetCore("SendNotification", {Title = "5xys Secret Message", Text = "Nexus tried to exploit us but failed, better luck next time.", Duration = 5})
